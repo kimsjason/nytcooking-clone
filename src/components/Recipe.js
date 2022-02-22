511f73259894c0b5273e307c729af7e80a40d8b0
Jason Kim <jason.s.kim95@gmail.com>
2022-02-22 16:55:56 -0500

    Add styled recipe page component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,5 +1,173 @@
+import { StyledRecipe } from "../styles/Recipe.styled";
+import { useLocation } from "react-router-dom";
+import {
+  BookmarkBorder,
+  Check,
+  Facebook,
+  Mail,
+  Pinterest,
+  Print,
+  StarBorder,
+  Twitter,
+} from "@mui/icons-material";
+import { CookingNote } from "./CookingNote";
+
 const Recipe = (props) => {
-  return <div className="recipe-page"></div>;
+  const location = useLocation();
+  const { recipe } = location.state || {};
+
+  return (
+    <StyledRecipe>
+      <div className="recipe-page">
+        <div className="recipe-content">
+          <div className="share-recipe">
+            <Mail className="mail-icon" />
+            <Pinterest className="pinterest-icon" />
+            <Facebook className="facebook-icon" />
+            <Twitter className="twitter-icon" />
+          </div>
+          <div className="recipe-information">
+            <div className="title">{recipe.title}</div>
+            <div className="author"> By {recipe.author}</div>
+            <div className="yield-time-save-print">
+              <div className="yield-and-time">
+                <div className="yield">
+                  <span>YIELD</span> {recipe.yield}
+                </div>
+                <div className="time">
+                  <span>TIME</span> {recipe.time}
+                </div>
+              </div>
+              <div className="save-share-recipe">
+                <div className="save-print-recipe">
+                  <div className="save-recipe">
+                    <BookmarkBorder className="bookmark-icon" />
+                    Save to Recipe Box
+                  </div>
+                  <div className="print-recipe">
+                    <Print className="print-icon" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="image-and-description">
+              <img
+                className="recipe-image"
+                src={recipe.img}
+                alt={`${recipe.title}`}
+              />
+              <div className="description">{recipe.description}</div>
+            </div>
+          </div>
+          <div className="tags-and-ratings">
+            <div className="tags">
+              {recipe.tags.map((tag) => {
+                return (
+                  <div key={tag} className="tag">
+                    {tag}
+                    {recipe.tags.indexOf(tag) === recipe.tags.length - 1 ? (
+                      ""
+                    ) : (
+                      <span className="tag">, </span>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="cooked-and-ratings">
+              <div className="mark-as-cooked">
+                <Check className="check-icon" /> Mark as <span> Cooked</span>
+              </div>
+              <span className="divider"></span>
+              <div className="ratings">
+                {recipe.ratings.length} ratings
+                <div className="stars">
+                  <StarBorder className="star-icon" />
+                  <StarBorder className="star-icon" />
+                  <StarBorder className="star-icon" />
+                  <StarBorder className="star-icon" />
+                  <StarBorder className="star-icon" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="instructions-and-cooking-notes">
+            <div className="col-1">
+              <div className="ingredients-container">
+                <div className="ingredients-header">INGREDIENTS</div>
+                <div className="ingredients">
+                  {recipe.ingredients.map((ingredient) => {
+                    return <div className="ingredient">{ingredient}</div>;
+                  })}
+                </div>
+              </div>
+              <div className="add-to-grocery-list">
+                Add to Your Grocery List
+              </div>
+              <div className="ingredient-substitution">
+                Ingredient Substitution Guide
+              </div>
+            </div>
+            <div className="col-2">
+              <div className="preparation">
+                <div className="preparation-header">PREPARATION</div>
+                <div className="steps">
+                  {recipe.instructions.map((step) => {
+                    return (
+                      <div className="step">
+                        <div className="step-number">
+                          Step {recipe.instructions.indexOf(step) + 1}
+                        </div>
+                        {step}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="mark-as-cooked-container">
+                Have you cooked this?
+                <div className="mark-as-cooked">
+                  <Check className="check-icon" />
+                  Mark as <span> Cooked</span>
+                </div>
+              </div>
+              <div className="cooking-notes">
+                <div className="cooking-notes-title">COOKING NOTES</div>
+                <input
+                  className="comment"
+                  type="text"
+                  placeholder="Share your notes with other cooks or leave a private note."
+                />
+                <div className="notes-container">
+                  <div className="notes-header">
+                    <div className="all">All</div>
+                    <div className="most-helpful">Most Helpful</div>
+                    <div className="private">Private</div>
+                  </div>
+                  <div className="notes">
+                    {recipe.notes.map((note) => {
+                      return <CookingNote note={note} />;
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="recommendations">
+        <div className="collection">
+          <div className="collection-header">TRENDING ON COOKING</div>
+          <div className="collection-recipes"></div>
+        </div>
+        <div className="collection">
+          <div className="collection-header">YOU RECENTLY VIEWED</div>
+          <div className="collection-recipes"></div>
+        </div>
+      </div>
+      ;
+    </StyledRecipe>
+  );
 };
 
 export default Recipe;
---
+++ src/styles/Recipe.styled.js
@@ -0,0 +1,366 @@
+import styled from "styled-components";
+
+const StyledRecipe = styled.div`
+  .recipe-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3rem;
+    background: white;
+  }
+
+  .recipe-content {
+    display: flex;
+    flex-direction: column;
+    width: 1000px;
+  }
+
+  .share-recipe {
+    display: flex;
+    justify-content: flex-end;
+    gap: 0.9rem;
+  }
+
+  .share-recipe > * {
+    width: 3.5rem;
+    height: 3.5rem;
+    padding: 0.8rem;
+    border-radius: 50%;
+    color: white;
+    cursor: pointer;
+  }
+
+  .mail-icon {
+    background: #b7b4b4;
+  }
+
+  .pinterest-icon {
+    background: #e34c5c;
+  }
+
+  .facebook-icon {
+    background: #6d81b8;
+  }
+
+  .twitter-icon {
+    background: #74b8ee;
+  }
+
+  .title {
+    margin-bottom: 1.5rem;
+    font-size: 4.2rem;
+    font-family: "Sorts Mill Goudy";
+  }
+
+  .author {
+    padding-bottom: 2.2rem;
+    border-bottom: 1px solid #cccccc;
+    margin-bottom: 2.4rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .yield-time-save-print {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .yield-and-time {
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+  }
+
+  .yield,
+  .time {
+    display: flex;
+    font-size: 1.5rem;
+  }
+
+  .yield span,
+  .time span {
+    width: 6rem;
+    font-family: Arvo;
+  }
+
+  .save-print-recipe {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .save-recipe:hover,
+  .print-recipe:hover {
+    background: #c83c26;
+  }
+
+  .save-recipe {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 170px;
+    height: 3.4rem;
+    border-radius: 3px;
+    font-size: 1.4rem;
+    color: white;
+    background: #e33d26;
+    cursor: pointer;
+  }
+
+  .bookmark-icon {
+    width: 2.5rem;
+    height: auto;
+  }
+
+  .print-recipe {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 4.5rem;
+    height: 3.4rem;
+    border-radius: 3px;
+    background: #e33d26;
+    color: white;
+    cursor: pointer;
+  }
+
+  .print-icon {
+    width: 2.5rem;
+    height: auto;
+  }
+
+  .image-and-description {
+    display: flex;
+    justify-content: space-between;
+    margin: 3rem 0;
+  }
+
+  .recipe-image {
+    width: 600px;
+  }
+
+  .description {
+    width: 370px;
+    font-size: 1.7rem;
+    font-family: "Sorts Mill Goudy";
+  }
+
+  .tags-and-ratings {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 3rem 0;
+    padding: 1.5rem 0;
+    border-top: 2px solid #222222;
+    border-bottom: 1px solid #cccccc;
+  }
+
+  .tags {
+    display: flex;
+  }
+
+  .tag {
+    white-space: pre;
+    font-size: 1.5rem;
+    color: #e33d26;
+    cursor: pointer;
+  }
+
+  .tag:hover {
+    color: #c83c26;
+    text-decoration: underline;
+  }
+
+  .tag span {
+    color: #222222;
+  }
+
+  .cooked-and-ratings {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
+
+  .mark-as-cooked {
+    display: flex;
+    align-items: center;
+    font-size: 1.5rem;
+    font-weight: normal;
+    cursor: pointer;
+  }
+
+  .mark-as-cooked:hover .check-icon {
+    color: white;
+    background: #b7b4b4;
+  }
+
+  .check-icon {
+    width: 2rem;
+    height: auto;
+    margin-right: 1rem;
+    border: 2px solid #b7b4b4;
+    border-radius: 50%;
+    padding: 1px;
+    color: #b7b4b4;
+  }
+
+  .mark-as-cooked span {
+    white-space: pre;
+    font-weight: bold;
+  }
+
+  .divider {
+    height: 3rem;
+    border: 0.25px solid #b7b4b4;
+  }
+
+  .ratings {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    font-size: 1.5rem;
+  }
+
+  .stars {
+    display: flex;
+    align-items: center;
+  }
+
+  .star-icon {
+    width: 2.25rem;
+    height: auto;
+    color: #b7b4b4;
+    cursor: pointer;
+  }
+
+  .instructions-and-cooking-notes {
+    display: flex;
+    justify-content: space-between;
+    gap: 5rem;
+  }
+
+  .col-1 {
+    flex: 1;
+  }
+
+  .col-2 {
+    flex: 2;
+  }
+
+  .ingredients-header,
+  .preparation-header {
+    padding-bottom: 3rem;
+    font-family: Arvo;
+    font-size: 1.3rem;
+  }
+
+  .ingredients {
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .add-to-grocery-list {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 4rem;
+    margin: 3.5rem 0;
+    border: 1px solid #222222;
+    border-radius: 3px;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+
+  .add-to-grocery-list:hover {
+    background: #222222;
+    color: white;
+  }
+
+  .ingredient-substitution {
+    font-size: 1.3rem;
+    color: #e33d26;
+    cursor: pointer;
+  }
+
+  .preparation {
+    font-size: 1.8rem;
+  }
+  .steps {
+    line-height: 2.5rem;
+  }
+
+  .step {
+    margin-bottom: 3rem;
+  }
+
+  .step-number {
+    padding-bottom: 1rem;
+    font-weight: bold;
+  }
+
+  .mark-as-cooked-container {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin: 5rem 0;
+    padding: 1.4rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    background: #f2f3ef;
+    cursor: pointer;
+  }
+
+  .mark-as-cooked-container:hover .check-icon {
+    color: white;
+    background: #b7b4b4;
+  }
+
+  .cooking-notes {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .cooking-notes-title {
+    padding-bottom: 1rem;
+    font-size: 1.3rem;
+    font-family: Arvo;
+  }
+
+  .comment {
+    display: flex;
+    margin-bottom: 4rem;
+    padding: 1.5rem;
+    border: 1px solid #dddddd;
+    font-family: "Libre Franklin";
+  }
+
+  .notes-header {
+    display: flex;
+    font-size: 1.6rem;
+    border-bottom: 1px solid #cccccc;
+    color: #666666;
+  }
+
+  .notes-header > * {
+    padding: 1.6rem;
+  }
+
+  .recommendations {
+    display: flex;
+    flex-direction: column;
+    width: 1000px;
+    padding: 7.5rem;
+  }
+
+  .collection-header {
+    font-size: 1.4rem;
+    font-weight: bold;
+    font-family: Arvo;
+  }
+`;
+
+export { StyledRecipe };
