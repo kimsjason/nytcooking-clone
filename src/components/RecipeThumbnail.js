1b1c0a8b8a44416e44ee092f18ea437fa9df6fdb
Jason Kim <jason.s.kim95@gmail.com>
2022-03-08 16:10:12 -0500

    Add login / signup hover box on bookmark icon

--- src/components/RecipeThumbnail.js
+++ src/components/RecipeThumbnail.js
@@ -1,51 +1,74 @@
 import { Bookmark, BookmarkBorder } from "@mui/icons-material";
 import { StyledRecipeThumbnail } from "../styles/RecipeThumbnail.styled";
 
-const RecipeThumbnail = (props) => {
-  const handleSaveRecipe = () => {
-    props.saveRecipe(props.recipe);
-  };
-
+const RecipeThumbnail = ({
+  user,
+  loggedIn,
+  recipe,
+  saveRecipe,
+  unsaveRecipe,
+  hideLogInPopup,
+  showLogInPopup,
+}) => {
+  const saved = user.savedRecipes.some(
+    (savedRecipe) => savedRecipe.title === recipe.title
+  );
   return (
     <StyledRecipeThumbnail>
       <div className="recipe-thumbnail">
         <img
-          src={props.recipe.img}
+          src={require(`../assets/${recipe.img}`)}
           className="recipe-image"
           alt="recipe thumbnail"
         />
         <div className="thumbnail-content">
           <div className="thumbnail-main">
-            <div className="title">{props.recipe.title}</div>
-            <div className="author">{props.recipe.author}</div>
+            <div className="title">{recipe.title}</div>
+            <div className="author">{recipe.author}</div>
           </div>
           <div className="thumbnail-footer">
-            <div className="time">{props.recipe.time}</div>
-            {props.user.savedRecipes.some(
-              (savedRecipe) => savedRecipe.title === props.recipe.title
-            ) ? (
-              <div
-                className="saved"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  handleSaveRecipe();
-                }}
-              >
-                <Bookmark className="bookmark-icon" />
-              </div>
-            ) : (
-              <div
-                className="not-saved"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  handleSaveRecipe();
-                }}
-              >
-                <BookmarkBorder className="bookmark-icon" />
-              </div>
-            )}
+            <div className="time">{recipe.time}</div>
+            <div
+              className="save-recipe"
+              onClick={
+                loggedIn
+                  ? (e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      hideLogInPopup();
+                      saved ? unsaveRecipe(recipe) : saveRecipe(recipe);
+                    }
+                  : (e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      showLogInPopup();
+                    }
+              }
+            >
+              {user.savedRecipes.some(
+                (savedRecipe) => savedRecipe.title === recipe.title
+              ) ? (
+                <div className="saved">
+                  <Bookmark className="bookmark-icon" />
+                </div>
+              ) : (
+                <div className="not-saved">
+                  <BookmarkBorder className="bookmark-icon" />
+                </div>
+              )}
+              {loggedIn ? (
+                ""
+              ) : (
+                <div className="description-box-container">
+                  <div className="log-in-sign-up">
+                    <span onClick={showLogInPopup}>Log in</span> or{" "}
+                    <span onClick={showLogInPopup}>Sign up</span> to save this
+                    recipe.
+                  </div>
+                  <div className="arrow"></div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- src/styles/RecipeThumbnail.styled.js
+++ src/styles/RecipeThumbnail.styled.js
@@ -37,15 +37,17 @@ const StyledRecipeThumbnail = styled.div`
     font-size: 1.3rem;
   }
 
-  .time {
-    font-size: 1.2rem;
-    color: #999999;
-  }
-
   .thumbnail-footer {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+
+  .time {
+    flex: 1;
+    display: flex;
+    font-size: 1.2rem;
+    color: #999999;
   }
 
   .saved,
@@ -62,6 +64,72 @@ const StyledRecipeThumbnail = styled.div`
     width: 2.5rem;
     height: auto;
     color: #999999;
+  }
+
+  .save-recipe {
+    position: relative;
+  }
+
+  .save-recipe:hover .description-box-container {
+    display: flex;
+  }
+
+  .description-box-container {
+    display: none;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    position: absolute;
+    width: 165px;
+    height: 7rem;
+    padding: 1.5rem;
+    right: -2rem;
+    bottom: 3rem;
+    border: 1px solid #ccc;
+    color: #222222;
+    background: white;
+    box-shadow: 0 0 10px rgb(0 0 0 / 10%);
+  }
+
+  .description-box {
+    margin-bottom: 1rem;
+    padding: 1.5rem;
+  }
+
+  .log-in-sign-up {
+    font-weight: bold;
+    font-size: 1.4rem;
+  }
+
+  .log-in-sign-up span {
+    color: #e33d26;
+  }
+
+  .log-in-sign-up span:hover {
+    text-decoration: underline;
+  }
+
+  .arrow {
+    position: absolute;
+    bottom: -1rem;
+    right: 2rem;
+    width: 0;
+    height: 0;
+    border-left: 1rem solid transparent;
+    border-right: 1rem solid transparent;
+    border-top: 1rem solid #ccc;
+  }
+
+  .arrow:after {
+    content: "";
+    width: 0;
+    height: 0;
+    border-left: 0.9rem solid transparent;
+    border-right: 0.9rem solid transparent;
+    border-top: 0.9rem solid white;
+    position: absolute;
+    bottom: 2px;
+    right: -9px;
   }
 `;
 
