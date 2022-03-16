944c0fc71627fdb3659f9a1aa10486da5e450860
Jason Kim <jason.s.kim95@gmail.com>
2022-03-16 14:33:53 -0400

    Update header styles for responsive design - mobile first

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { StyledHeader } from "../styles/Header.styled";
 import { ReactComponent as Logo } from "../assets/nyt-cooking-logo.svg";
-import { Close, Search, Settings } from "@mui/icons-material";
+import { Close, Inbox, Menu, Search, Settings } from "@mui/icons-material";
 import reactStringReplace from "react-string-replace";
 
 const Header = ({
@@ -59,90 +59,107 @@ const Header = ({
   return (
     <StyledHeader>
       <div className="header">
-        <Link to="/" className="nyt-cooking-logo">
-          <Logo />
-        </Link>
-        <div className="extended-search-container">
-          <div className="search">
-            <Search className="search-icon" />
-            <input
-              type="text"
-              className="search-bar"
-              placeholder="What would you like to cook?"
-              onClick={expandSearchBar}
-              onChange={(e) => {
-                setSearchQuery(e.target.value);
-                searchRecipes(e.target.value);
-                if (searchResults.length > 0) {
-                  showSearchResults();
-                }
-              }}
-            />
-            <Close className="close-icon" />
-          </div>
-
-          <div className="tabs">
-            <Link to="/weeknight" className="weeknight">
-              WEEKNIGHT
-            </Link>
-            <div
-              className="grocery-list"
-              onClick={loggedIn ? showGroceryList : showLogInPopup}
-            >
-              YOUR GROCERY LIST
-            </div>
-          </div>
-          <div className="search-results-preview hidden">
-            {searchResults.slice(0, 5).map((searchResult) => {
-              return (
-                <Link
-                  to={`/recipe/${searchResult.title}`}
-                  className="search-result"
-                >
-                  <img
-                    src={require(`../assets/${searchResult.img}`)}
-                    alt=""
-                    className="recipe-image-icon"
-                  />
-                  <div className="search-result-title">
-                    {reactStringReplace(
-                      searchResult.title,
-                      searchQuery,
-                      (match, i) => {
-                        return <span className="bold">{match}</span>;
-                      }
-                    )}
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
-
-        <div
-          className="recipe-box-container"
-          onClick={loggedIn ? hideLogInPopup : showLogInPopup}
-        >
-          <Link to="/recipe-box" className="recipe-box">
-            YOUR RECIPE BOX
-            {loggedIn ? "" : <span>Log In</span>}
+        <div className="header-mobile">
+          <Menu className="menu-icon" />
+          <Link to="/" className="nyt-cooking-logo">
+            <Logo />
           </Link>
+          <div className="recipe-box-and-search">
+            <Link to="/recipe-box">
+              <Inbox className="recipe-box-icon" />
+            </Link>
+            <div className="divider"></div>
+            <Search className="search-icon" />
+          </div>
         </div>
-        {loggedIn ? (
-          <div className="settings">
-            <Settings className="settings-icon" />
-            <div className="settings-expanded">
-              <div className="account">Manage Your Account</div>
-              <div className="log-out" onClick={signOutUser}>
-                Log Out
+        <div className="header-web hidden">
+          <Link to="/" className="nyt-cooking-logo">
+            <Logo />
+          </Link>
+          <div className="extended-search-container">
+            <div className="search">
+              <Search className="search-icon" />
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="What would you like to cook?"
+                onClick={expandSearchBar}
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                  searchRecipes(e.target.value);
+                  if (searchResults.length > 0) {
+                    showSearchResults();
+                  }
+                }}
+              />
+              <Close className="close-icon" />
+            </div>
+
+            <div className="tabs">
+              <Link to="/weeknight" className="weeknight">
+                WEEKNIGHT
+              </Link>
+              <div
+                className="grocery-list"
+                onClick={loggedIn ? showGroceryList : showLogInPopup}
+              >
+                <span className="your hidden">YOUR </span>GROCERY LIST
               </div>
             </div>
+            <div className="search-results-preview hidden">
+              {searchResults.slice(0, 5).map((searchResult) => {
+                return (
+                  <Link
+                    to={`/recipe/${searchResult.title}`}
+                    className="search-result"
+                  >
+                    <img
+                      src={require(`../assets/${searchResult.img}`)}
+                      alt=""
+                      className="recipe-image-icon"
+                    />
+                    <div className="search-result-title">
+                      {reactStringReplace(
+                        searchResult.title,
+                        searchQuery,
+                        (match, i) => {
+                          return <span className="bold">{match}</span>;
+                        }
+                      )}
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
           </div>
-        ) : (
-          <Link to="/subscribe" className="subscribe">
-            SUBSCRIBE
-          </Link>
-        )}
+
+          <div
+            className="recipe-box-container"
+            onClick={loggedIn ? hideLogInPopup : showLogInPopup}
+          >
+            <Link to="/recipe-box" className="recipe-box">
+              <div className="main-text">
+                <span className="your hidden">YOUR </span> RECIPE BOX
+              </div>
+              {loggedIn ? "" : <span>Log In</span>}
+            </Link>
+          </div>
+          {loggedIn ? (
+            <div className="settings">
+              <Settings className="settings-icon" />
+              <div className="settings-expanded">
+                <div className="account">Manage Your Account</div>
+                <div className="log-out" onClick={signOutUser}>
+                  Log Out
+                </div>
+              </div>
+            </div>
+          ) : (
+            <Link to="/subscribe" className="subscribe">
+              SUBSCRIBE
+            </Link>
+          )}
+        </div>
       </div>
     </StyledHeader>
   );
--- src/styles/Header.styled.js
+++ src/styles/Header.styled.js
@@ -4,234 +4,312 @@ const StyledHeader = styled.div`
   z-index: 50;
 
   .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     position: fixed;
     width: 100%;
     top: 0;
     border-bottom: 1px solid #cccccc;
     background: white;
-    color: #666666;
   }
 
-  .nyt-cooking-logo {
+  .header-mobile {
     display: flex;
-    align-self: stretch;
-    padding: 1.5rem 4.5rem 1.5rem 2.5rem;
-    border-right: 1px solid #cccccc;
-  }
-
-  .extended-search-container {
-    flex: 1;
-    align-self: stretch;
-    display: flex;
-    position: relative;
-  }
-
-  .search {
-    display: flex;
-    align-items: center;
-    width: 325px;
-    padding: 0 1.5rem;
-    background: white;
-    transition: 0.5s width;
-    z-index: 10;
-  }
-
-  .search-icon {
-    width: 25px;
-    height: 25px;
-    color: #9b9b9b;
-  }
-
-  .expanded {
-    width: 100%;
-  }
-
-  .search-bar {
-    flex: 1;
-    border: none;
-    padding: 5px;
-    font-size: 1.8rem;
-    font-family: "Libre Franklin";
-    color: #666666;
-  }
-
-  .search-bar:focus {
-    outline: none;
-  }
-
-  .close-icon {
-    position: absolute;
-    right: 2rem;
-    width: 2rem;
-    height: auto;
-    padding: 1px;
-    border-radius: 50%;
-    background: #ccc;
-    color: white;
-    opacity: 0;
-    transition: opacity 1s;
-    cursor: pointer;
-  }
-
-  .fade-in {
-    opacity: 1;
-  }
-
-  .tabs {
-    display: flex;
-    position: absolute;
-    right: 0;
-    height: 100%;
-  }
-
-  .weeknight,
-  .grocery-list,
-  .subscribe {
-    align-self: stretch;
-    display: flex;
-    align-items: center;
-    margin: 0 1.2rem;
-    text-align: center;
-    font-weight: bold;
-    font-size: 1.1rem;
-    cursor: pointer;
-  }
-
-  .subscribe {
-    margin: 0 2.5rem;
-    color: #e33d26;
-  }
-
-  .weeknight:hover,
-  .grocery-list:hover {
-    margin-top: 4px;
-    border-bottom: 4px solid #666666;
-  }
-
-  .search-results-preview {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    width: 100%;
-    top: 61px;
-    right: -1px;
-    border: 1px solid #cccccc;
-    background: #f2f3ef;
-    box-sizing: content-box;
-  }
-
-  .search-result {
-    display: flex;
+    justify-content: space-between;
     align-items: center;
     gap: 1rem;
-    padding: 1rem 1.5rem;
-    font-size: 2rem;
+    height: 44px;
+    padding: 0 1rem;
+  }
+
+  .nyt-cooking-logo svg {
+    width: 130px;
+    height: 24px;
+  }
+
+  .nyt-cooking-logo path {
+    fill: black;
+  }
+
+  .recipe-box-and-search {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .divider {
+    border-right: 1px solid #ccc;
+  }
+
+  .menu-icon,
+  .search-icon {
+    width: 2.5rem;
+    height: auto;
+    color: black;
     cursor: pointer;
   }
 
-  .search-result:hover {
-    background: #767676;
-    color: white;
-  }
-  .bold {
-    font-weight: bold;
-  }
-
-  .recipe-image-icon {
-    width: 5rem;
+  .recipe-box-icon {
+    width: 2.25rem;
     height: auto;
   }
 
-  .subscribe:hover {
-    margin-top: 4px;
-    border-bottom: 4px solid #e33d26;
-  }
-
-  .recipe-box-container {
-    align-self: stretch;
-    display: flex;
-    align-items: center;
-    border: 1px solid #cccccc;
-    border-radius: 2.5px 2.5px 0 0;
-    border-bottom: none;
-    margin-top: 1rem;
-    padding: 0 2rem;
-    cursor: pointer;
-  }
-
-  .recipe-box-container:hover {
-    background: #f2f3ef;
-  }
-
-  .recipe-box {
-    align-self: flex-start;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 1rem;
-    padding-top: 1.35rem;
-    text-align: center;
-    font-weight: bold;
-    font-size: 1.1rem;
+  .menu-icon:hover,
+  .recipe-box-icon:hover,
+  .search-icon:hover {
     color: #e33d26;
-  }
-
-  .recipe-box:hover {
-    color: #c83c26;
-  }
-
-  .recipe-box span {
-    font-weight: normal;
-    color: #222222;
-  }
-
-  .settings {
-    align-self: stretch;
-    display: flex;
-    align-items: center;
-    position: relative;
-    padding: 0 1.5rem;
-    cursor: pointer;
-  }
-
-  .settings-icon {
-    width: 20px;
-    height: 20px;
-    color: #9b9b9b;
-  }
-
-  .settings:hover .settings-icon {
-    color: #e33d26;
-  }
-
-  .settings:hover .settings-expanded {
-    display: flex;
-  }
-
-  .settings-expanded {
-    display: none;
-    flex-direction: column;
-    position: absolute;
-    width: 156px;
-    bottom: -80px;
-    right: 2.5rem;
-    font-size: 1.2rem;
-    background: #f2f3ef;
-    z-index: 10;
-  }
-
-  .settings-expanded > * {
-    width: 100%;
-    height: 4rem;
-    padding: 0.8rem 1.5rem;
-    border: 1px solid #ccc;
-    cursor: pointer;
   }
 
   .hidden {
     display: none;
+  }
+
+  @media only screen and (min-width: 1024px) {
+    .header-mobile {
+      display: none;
+    }
+
+    .header-web {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 61px;
+      color: #666666;
+    }
+
+    .nyt-cooking-logo {
+      display: flex;
+      align-self: stretch;
+      padding: 1.5rem 4.5rem 1.5rem 2.5rem;
+      border-right: 1px solid #cccccc;
+    }
+
+    .nyt-cooking-logo svg {
+      width: 168px;
+      height: auto;
+    }
+
+    .nyt-cooking-logo path {
+      fill: #e33d26;
+    }
+
+    .extended-search-container {
+      flex: 1;
+      align-self: stretch;
+      display: flex;
+      position: relative;
+    }
+
+    .search {
+      display: flex;
+      align-items: center;
+      width: 325px;
+      padding: 0 1.5rem;
+      background: white;
+      transition: 0.5s width;
+      z-index: 10;
+    }
+
+    .search-icon {
+      width: 25px;
+      height: 25px;
+      color: #9b9b9b;
+    }
+
+    .expanded {
+      width: 100%;
+    }
+
+    .search-bar {
+      flex: 1;
+      border: none;
+      padding: 5px;
+      font-size: 1.8rem;
+      font-family: "Libre Franklin";
+      color: #666666;
+    }
+
+    .search-bar:focus {
+      outline: none;
+    }
+
+    .close-icon {
+      position: absolute;
+      right: 2rem;
+      width: 2rem;
+      height: auto;
+      padding: 1px;
+      border-radius: 50%;
+      background: #ccc;
+      color: white;
+      opacity: 0;
+      transition: opacity 1s;
+      cursor: pointer;
+    }
+
+    .fade-in {
+      opacity: 1;
+    }
+
+    .tabs {
+      display: flex;
+      position: absolute;
+      right: 0;
+      height: 100%;
+    }
+
+    .weeknight,
+    .grocery-list,
+    .subscribe {
+      align-self: stretch;
+      display: flex;
+      align-items: center;
+      margin: 0 1.2rem;
+      text-align: center;
+      font-weight: bold;
+      font-size: 1.1rem;
+      cursor: pointer;
+    }
+
+    .subscribe {
+      margin: 0 2.5rem;
+      color: #e33d26;
+    }
+
+    .weeknight:hover,
+    .grocery-list:hover {
+      margin-top: 4px;
+      border-bottom: 4px solid #666666;
+    }
+
+    .search-results-preview {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      width: 100%;
+      top: 61px;
+      right: -1px;
+      border: 1px solid #cccccc;
+      border-top: none;
+      background: #f2f3ef;
+      box-sizing: content-box;
+    }
+
+    .search-result {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      padding: 1rem 1.5rem;
+      font-size: 2rem;
+      cursor: pointer;
+    }
+
+    .search-result:hover {
+      background: #767676;
+      color: white;
+    }
+    .bold {
+      font-weight: bold;
+    }
+
+    .recipe-image-icon {
+      width: 5rem;
+      height: auto;
+    }
+
+    .subscribe:hover {
+      margin-top: 4px;
+      border-bottom: 4px solid #e33d26;
+    }
+
+    .recipe-box-container {
+      align-self: stretch;
+      display: flex;
+      align-items: center;
+      border: 1px solid #cccccc;
+      border-radius: 2.5px 2.5px 0 0;
+      border-bottom: none;
+      margin-top: 1rem;
+      padding: 0 2rem;
+      cursor: pointer;
+    }
+
+    .recipe-box-container:hover {
+      background: #f2f3ef;
+    }
+
+    .recipe-box {
+      align-self: flex-start;
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 1rem;
+      padding-top: 1.35rem;
+      text-align: center;
+      font-weight: bold;
+      font-size: 1.1rem;
+      color: #e33d26;
+    }
+
+    .recipe-box:hover {
+      color: #c83c26;
+    }
+
+    .recipe-box span {
+      font-weight: normal;
+      color: #222222;
+    }
+
+    .recipe-box .your {
+      color: #e33d26;
+      font-weight: bold;
+    }
+
+    .settings {
+      align-self: stretch;
+      display: flex;
+      align-items: center;
+      position: relative;
+      padding: 0 1.5rem;
+      cursor: pointer;
+    }
+
+    .settings-icon {
+      width: 20px;
+      height: 20px;
+      color: #9b9b9b;
+    }
+
+    .settings:hover .settings-icon {
+      color: #e33d26;
+    }
+
+    .settings:hover .settings-expanded {
+      display: flex;
+    }
+
+    .settings-expanded {
+      display: none;
+      flex-direction: column;
+      position: absolute;
+      width: 156px;
+      bottom: -80px;
+      right: 2.5rem;
+      font-size: 1.2rem;
+      background: #f2f3ef;
+      z-index: 10;
+    }
+
+    .settings-expanded > * {
+      width: 100%;
+      height: 4rem;
+      padding: 0.8rem 1.5rem;
+      border: 1px solid #ccc;
+      cursor: pointer;
+    }
+
+    @media only screen and (min-width: 1200px) {
+      .grocery-list span,
+      .recipe-box span {
+        display: inline-block;
+      }
+    }
   }
 `;
 
