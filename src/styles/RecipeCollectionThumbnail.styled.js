59a93e285f1d92a66e94a941af937c7a747c6370
Jason Kim <jason.s.kim95@gmail.com>
2022-03-16 22:36:43 -0400

    Add mobile responsive media query

--- src/styles/RecipeCollectionThumbnail.styled.js
+++ src/styles/RecipeCollectionThumbnail.styled.js
@@ -4,15 +4,15 @@ const StyledRecipeCollectionThumbnail = styled.div`
   .recipe-collection-thumbnail {
     display: flex;
     flex-direction: column;
-    width: 475px;
-    height: 500px;
+    width: 300px;
+    height: 325px;
     background: white;
     cursor: pointer;
   }
 
   .recipe-collection-image {
     object-fit: cover;
-    height: 325px;
+    height: 195px;
   }
 
   .thumbnail-content {
@@ -25,7 +25,7 @@ const StyledRecipeCollectionThumbnail = styled.div`
   .title {
     margin: 20px 0;
     text-align: center;
-    font-size: 2.4rem;
+    font-size: 1.7rem;
   }
 
   .save-recipes {
@@ -33,7 +33,7 @@ const StyledRecipeCollectionThumbnail = styled.div`
     align-items: center;
     padding: 0.75rem 1.2rem;
     border-radius: 3px;
-    font-size: 1.4rem;
+    font-size: 1.3rem;
     color: white;
     background: #e33d26;
   }
@@ -45,6 +45,40 @@ const StyledRecipeCollectionThumbnail = styled.div`
   .bookmark-icon {
     width: 2.5rem;
     height: auto;
+  }
+
+  @media only screen and (min-width: 510px) {
+    .recipe-collection-thumbnail {
+      width: 360px;
+      height: 390px;
+    }
+
+    .recipe-collection-image {
+      height: 235px;
+    }
+
+    .title {
+      font-size: 2.2rem;
+    }
+
+    .save-recipes {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media only screen and (min-width: 1020px) {
+    .recipe-collection-thumbnail {
+      width: 475px;
+      height: 500px;
+    }
+
+    .recipe-collection-image {
+      height: 325px;
+    }
+
+    .title {
+      font-size: 2.4rem;
+    }
   }
 `;
 
